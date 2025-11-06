d48ddd3e1beda8a82a8459edbc8ed540bfe0230d
meron2028-IT-class <meron2028@hmacademy.org>
2025-11-06 09:37:57 +0300

    Create Divisibility Check.py

---
+++ Divisibility Check.py
@@ -0,0 +1,5 @@
+divisible = int(input("Tell me a number and I will tell you if it is divisible by 5 or not: "))
+if divisible % 5 == 0:
+    print("It is divisible by 5!")
+else:
+    print("it is not divisble by 5")
